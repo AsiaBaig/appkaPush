5909b23c5a4ffc5c70692d9cc44f16d596444dd6
Andry <gaxeyat298@chordmi.com>
2020-06-01 17:34:05 +0300

    Updates

---
+++ precache-manifest.e6b440ff303e4070ee2d4f5c2af5d58e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c6c0c35fe267bc7aefe04a11be0c11",
+    "url": "/appkaPush/index.html"
+  },
+  {
+    "revision": "30a8f46674fa3432f0d6",
+    "url": "/appkaPush/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "8643c931d3f9a88b1be6",
+    "url": "/appkaPush/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "30a8f46674fa3432f0d6",
+    "url": "/appkaPush/static/js/2.502d6bc3.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/appkaPush/static/js/2.502d6bc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8643c931d3f9a88b1be6",
+    "url": "/appkaPush/static/js/main.8d34d128.chunk.js"
+  },
+  {
+    "revision": "85b4e4c130bae042b182",
+    "url": "/appkaPush/static/js/runtime-main.85d59aab.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/appkaPush/static/media/persik.4e1ec840.png"
+  }
+]);
